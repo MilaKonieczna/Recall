6a40f268675a50541b265e3921601da2fbbfcffa
MilaKonieczna <milkakm2003@gmail.com>
2024-06-22 20:57:32 +0200

    added audio :)

--- app/src/main/java/com/example/recall/main/Home.kt
+++ app/src/main/java/com/example/recall/main/Home.kt
@@ -1,5 +1,6 @@
 package com.example.recall.main
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -12,12 +13,25 @@ import com.example.recall.sst.StartSST
 import com.example.recall.st.StartST
 
 class Home : AppCompatActivity() {
+    private lateinit var mediaPlayerBNT: MediaPlayer
+    private lateinit var mediaPlayerDST: MediaPlayer
+    private lateinit var mediaPlayerSST: MediaPlayer
+    private lateinit var mediaPlayerST: MediaPlayer
+    private lateinit var mediaPlayerRES: MediaPlayer
+
+
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mediaPlayerBNT = MediaPlayer.create(this, R.raw.bnt)
+        mediaPlayerDST = MediaPlayer.create(this, R.raw.dst)
+        mediaPlayerSST = MediaPlayer.create(this, R.raw.sst)
+        mediaPlayerST = MediaPlayer.create(this, R.raw.st)
+        mediaPlayerRES = MediaPlayer.create(this, R.raw.res)
 
         val fragmentToLoad = intent.getStringExtra("fragmentToLoad")
         if (fragmentToLoad != null) {
@@ -30,11 +44,24 @@ class Home : AppCompatActivity() {
 
         binding.menu.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.nav_bnt -> replaceFragment(StartBNT())
-                R.id.nav_dst -> replaceFragment(StartDST())
-                R.id.nav_sst -> replaceFragment(StartSST())
-                R.id.nav_st -> replaceFragment(StartST())
-                R.id.nav_res -> replaceFragment(PastResults())
+                R.id.nav_bnt -> {
+                    replaceFragment(StartBNT())
+                    mediaPlayerBNT.start()
+                }
+                R.id.nav_dst -> {
+                    replaceFragment(StartDST())
+                    mediaPlayerDST.start()
+                }
+                R.id.nav_sst -> {
+                    replaceFragment(StartSST())
+                    mediaPlayerSST.start()
+                }
+                R.id.nav_st -> {
+                    replaceFragment(StartST())
+                    mediaPlayerST.start()
+                }
+                R.id.nav_res -> {replaceFragment(PastResults())
+                mediaPlayerRES.start()}
             }
             true
         }
@@ -44,5 +71,13 @@ class Home : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.frame, fragment)
             .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayerBNT.release()
+        mediaPlayerDST.release()
+        mediaPlayerSST.release()
+        mediaPlayerST.release()
     }
 }
